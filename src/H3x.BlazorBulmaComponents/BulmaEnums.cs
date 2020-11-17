d59812e3aad325c30b7e3f046f49001c1ffb6f83
Sander in 't Hout <sander@inthout.eu>
2020-11-17 11:32:26 +0100

    Update project to .NET 5.0 and remove unneeded usings

--- src/H3x.BlazorBulmaComponents/BulmaEnums.cs
+++ src/H3x.BlazorBulmaComponents/BulmaEnums.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace H3x.BlazorBulmaComponents
+﻿namespace H3x.BlazorBulmaComponents
 {
     public enum BulmaColors
     {
